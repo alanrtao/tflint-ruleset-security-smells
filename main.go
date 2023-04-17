9610ff1154a5ea8221f7fafd8308d3217043beeb
srhee34 <60160057+joshrhee@users.noreply.github.com>
2023-04-16 22:20:44 -0400

    First commit to add Invalid_IP_Address_Binding Rule

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/alanrtao/tflint-ruleset-secsmells/rules"
-
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 )
@@ -13,8 +12,9 @@ func main() {
 			Name:    "Security Smells Linter Ruleset",
 			Version: "0.1.0",
 			Rules: []tflint.Rule{
-				rules.NewPasswordStrengthRuleType(),
-				rules.NewNoHardcodedSecretRuleType(),
+				// rules.NewPasswordStrengthRuleType(),
+				// rules.NewNoHardcodedSecretRuleType(),
+				rules.NewInvalidIPAddressBindingRuleType(),
 			},
 		},
 	})
